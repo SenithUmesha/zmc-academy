0da01c673a309ba6666aef5fcadecaa928991cb6
Senith Umesha <90299964+SenithUmesha@users.noreply.github.com>
2026-09-17 22:39:15 +0530

    refactor: load academy news in one query

--- WF CW2 TEST/News1.cs
+++ WF CW2 TEST/News1.cs
@@ -1,13 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Drawing;
-using System.Data;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
+using System;
 using System.Data.SqlClient;
+using System.Windows.Forms;
+using WF_CW2_TEST.Infrastructure;
 
 namespace WF_CW2_TEST
 {
@@ -20,107 +14,34 @@ namespace WF_CW2_TEST
 
         private void News1_Load(object sender, EventArgs e)
         {
-            string A = "A"; string B = "B"; string C = "C"; string D = "D";
-            string connectionString;
-            SqlConnection cnn;
-
-            connectionString = @"Data Source = SENITHUMESHA\SQLEXPRESS;Initial catalog = ZMC_Academy;User ID=admin;Password=admin";
-
-            cnn = new SqlConnection(connectionString);
-            cnn.Open();
-            string sql1 = "Select News_name from News where News_id = '" + A + "'";
-            SqlCommand cmd1 = new SqlCommand(sql1, cnn);
-            SqlDataReader reader1 = cmd1.ExecuteReader();
-
-            if (reader1.Read())
+            try
             {
-                txtnews1.Text = reader1.GetValue(0).ToString();
+                using (SqlConnection connection = Database.OpenConnection())
+                using (var command = new SqlCommand(
+                    "SELECT News_id, News_name, News_date FROM News WHERE News_id IN ('A','B','C','D') ORDER BY News_id", connection))
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        string id = Convert.ToString(reader["News_id"]);
+                        string headline = Convert.ToString(reader["News_name"]);
+                        string date = Convert.ToString(reader["News_date"]);
+
+                        switch (id)
+                        {
+                            case "A": txtnews1.Text = headline; txtnewsdate1.Text = date; break;
+                            case "B": txtnews2.Text = headline; txtnewsdate2.Text = date; break;
+                            case "C": txtnews3.Text = headline; txtnewsdate3.Text = date; break;
+                            case "D": txtnews4.Text = headline; txtnewsdate4.Text = date; break;
+                        }
+                    }
+                }
             }
-            cnn.Close();
-
-            cnn = new SqlConnection(connectionString);
-            cnn.Open();
-            string sql2 = "Select News_name from News where News_id = '" + B + "'";
-            SqlCommand cmd2 = new SqlCommand(sql2, cnn);
-            SqlDataReader reader2 = cmd2.ExecuteReader();
-
-            if (reader2.Read())
+            catch (Exception ex)
             {
-                txtnews2.Text = reader2.GetValue(0).ToString();
+                MessageBox.Show("News could not be loaded. " + ex.Message,
+                    "News", MessageBoxButtons.OK, MessageBoxIcon.Warning);
             }
-            cnn.Close();
-
-            cnn = new SqlConnection(connectionString);
-            cnn.Open();
-            string sql3 = "Select News_name from News where News_id = '" + C + "'";
-            SqlCommand cmd3 = new SqlCommand(sql3, cnn);
-            SqlDataReader reader3 = cmd3.ExecuteReader();
-
-            if (reader3.Read())
-            {
-                txtnews3.Text = reader3.GetValue(0).ToString();
-            }
-            cnn.Close();
-
-            cnn = new SqlConnection(connectionString);
-            cnn.Open();
-            string sql4 = "Select News_name from News where News_id = '" + D + "'";
-            SqlCommand cmd4 = new SqlCommand(sql4, cnn);
-            SqlDataReader reader4 = cmd4.ExecuteReader();
-
-            if (reader4.Read())
-            {
-                txtnews4.Text = reader4.GetValue(0).ToString();
-            }
-            cnn.Close();
-
-            cnn = new SqlConnection(connectionString);
-            cnn.Open();
-            string sql5 = "Select News_date from News where News_id = '" + A + "'";
-            SqlCommand cmd5 = new SqlCommand(sql5, cnn);
-            SqlDataReader reader5 = cmd5.ExecuteReader();
-
-            if (reader5.Read())
-            {
-                txtnewsdate1.Text = reader5.GetValue(0).ToString();
-            }
-            cnn.Close();
-
-            cnn = new SqlConnection(connectionString);
-            cnn.Open();
-            string sql6 = "Select News_date from News where News_id = '" + B + "'";
-            SqlCommand cmd6 = new SqlCommand(sql6, cnn);
-            SqlDataReader reader6 = cmd6.ExecuteReader();
-
-            if (reader6.Read())
-            {
-                txtnewsdate2.Text = reader6.GetValue(0).ToString();
-            }
-            cnn.Close();
-
-            cnn = new SqlConnection(connectionString);
-            cnn.Open();
-            string sql7 = "Select News_date from News where News_id = '" + C + "'";
-            SqlCommand cmd7 = new SqlCommand(sql7, cnn);
-            SqlDataReader reader7 = cmd7.ExecuteReader();
-
-            if (reader7.Read())
-            {
-                txtnewsdate3.Text = reader7.GetValue(0).ToString();
-            }
-            cnn.Close();
-
-            cnn = new SqlConnection(connectionString);
-            cnn.Open();
-            string sql8 = "Select News_date from News where News_id = '" + D + "'";
-            SqlCommand cmd8 = new SqlCommand(sql8, cnn);
-            SqlDataReader reader8 = cmd8.ExecuteReader();
-
-            if (reader8.Read())
-            {
-                txtnewsdate4.Text = reader8.GetValue(0).ToString();
-            }
-            cnn.Close();
         }
     }
 }
