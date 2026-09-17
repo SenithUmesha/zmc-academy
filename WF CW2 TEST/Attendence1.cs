4816c297b41feb837d5e9c04c7a8cc7cb0e85c55
Senith Umesha <90299964+SenithUmesha@users.noreply.github.com>
2026-09-17 22:38:00 +0530

    security: parameterize attendance lookup

--- WF CW2 TEST/Attendence1.cs
+++ WF CW2 TEST/Attendence1.cs
@@ -1,13 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Drawing;
+using System;
 using System.Data;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
 using System.Data.SqlClient;
+using System.Windows.Forms;
+using WF_CW2_TEST.Infrastructure;
 
 namespace WF_CW2_TEST
 {
@@ -20,43 +15,25 @@ namespace WF_CW2_TEST
 
         private void btnsubmit_Click(object sender, EventArgs e)
         {
-            if (comboBox1.SelectedIndex == 0)
+            string table = comboBox1.SelectedIndex == 0 ? "Attendence1" : "Attendence2";
+
+            try
             {
-                string connectionString;
-                SqlConnection cnn;
-
-                connectionString = @"Data Source = SENITHUMESHA\SQLEXPRESS; Initial Catalog = ZMC_Academy; User ID = admin; Password = admin";
-
-                cnn = new SqlConnection(connectionString);
-
-                cnn.Open();
-                String sql = "Select * from Attendence1 where Id = '" + Signin1.signinID+ "'";
-                SqlCommand cmd = new SqlCommand(sql, cnn);
-
-                SqlDataAdapter ada = new SqlDataAdapter(cmd);
-                DataTable dataTable = new DataTable();
-                ada.Fill(dataTable);
-                dataGridView1.DataSource = dataTable;
-                cnn.Close();
+                using (SqlConnection connection = Database.OpenConnection())
+                using (var command = new SqlCommand(
+                    "SELECT Id, [date], Attended FROM " + table + " WHERE Id = @Id ORDER BY [date] DESC", connection))
+                using (var adapter = new SqlDataAdapter(command))
+                {
+                    command.Parameters.Add("@Id", SqlDbType.VarChar, 10).Value = Signin1.signinID;
+                    var dataTable = new DataTable();
+                    adapter.Fill(dataTable);
+                    dataGridView1.DataSource = dataTable;
+                }
             }
-            else
+            catch (Exception ex)
             {
-                string connectionString;
-                SqlConnection cnn;
-
-                connectionString = @"Data Source = SENITHUMESHA\SQLEXPRESS; Initial Catalog = ZMC_Academy; User ID = admin; Password = admin";
-
-                cnn = new SqlConnection(connectionString);
-
-                cnn.Open();
-                String sql = "Select * from Attendence2 where Id = '" + Signin1.signinID + "'";
-                SqlCommand cmd = new SqlCommand(sql, cnn);
-
-                SqlDataAdapter ada = new SqlDataAdapter(cmd);
-                DataTable dataTable = new DataTable();
-                ada.Fill(dataTable);
-                dataGridView1.DataSource = dataTable;
-                cnn.Close();
+                MessageBox.Show("Attendance could not be loaded. " + ex.Message,
+                    "Attendance", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
         }
     }
